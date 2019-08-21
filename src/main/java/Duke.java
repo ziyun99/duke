621199c2e23841070f922378dd4786255af57f88
ziyun99 <ziyun99@users.noreply.github.com>
2019-08-21 21:47:32 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        printLine();
+        printIndent();
+        System.out.println("Hello! I'm Duke");
+        printIndent();
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("");
+
+        String inData;
+        Scanner scan = new Scanner( System.in );
+        inData = scan.nextLine();
+        while (!inData.equals("bye")) {
+            printLine();
+            printIndent();
+            System.out.println(inData);
+            printLine();
+            System.out.println("");
+            inData = scan.nextLine();
+        }
+        printLine();
+        printIndent();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        printIndent();
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printIndent() {
+        int space = 4;
+        System.out.printf("%" + space + "s" , "");
     }
 }
