d858895011192de7b674db2277a6b37f067ba0f6
ziyun99 <ziyun99@users.noreply.github.com>
2019-08-22 09:20:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,31 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    static String[] myTasks = new String[100];
+    static int num = 0;
+
     public static void main(String[] args) {
 
+        printHello();
+
+        String inData;
+        Scanner scan = new Scanner(System.in);
+        inData = scan.nextLine();
+        while (!inData.equals("bye")) {
+            if (inData.equals("list")) {
+                listTask();
+            } else {
+                addTask(inData);
+                num ++;
+            }
+            System.out.println("");
+            inData = scan.nextLine();
+        }
+        printBye();
+    }
+
+    public static void printHello() {
         printLine();
         printIndent();
         System.out.println("Hello! I'm Duke");
@@ -10,18 +33,9 @@ public class Duke {
         System.out.println("What can I do for you?");
         printLine();
         System.out.println("");
+    }
 
-        String inData;
-        Scanner scan = new Scanner( System.in );
-        inData = scan.nextLine();
-        while (!inData.equals("bye")) {
-            printLine();
-            printIndent();
-            System.out.println(inData);
-            printLine();
-            System.out.println("");
-            inData = scan.nextLine();
-        }
+    public static void printBye() {
         printLine();
         printIndent();
         System.out.println("Bye. Hope to see you again soon!");
@@ -29,12 +43,33 @@ public class Duke {
     }
 
     public static void printLine() {
-        printIndent();
+        int space = 4;
+        System.out.printf("%" + space + "s", "");
         System.out.println("____________________________________________________________");
     }
 
     public static void printIndent() {
-        int space = 4;
-        System.out.printf("%" + space + "s" , "");
+        int space = 5;
+        System.out.printf("%" + space + "s", "");
     }
+
+    public static void listTask() {
+        printLine();
+        for (int i = 0; i < num; i++) {
+            printIndent();
+            int j = i + 1;
+            System.out.println(j + ". " + myTasks[i]);
+        }
+        printLine();
+    }
+
+    public static void addTask(String task) {
+        myTasks[num] = task;
+        printLine();
+        printIndent();
+        System.out.println("added: " + task);
+        printLine();
+    }
+
 }
+
