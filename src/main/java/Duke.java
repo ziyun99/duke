7e9a76e97ba5c694b75b70a447f1f0cb34b9df63
ziyun99 <ziyun99@users.noreply.github.com>
2019-08-22 18:08:21 +0800

    Level-3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    static String[] myTasks = new String[100];
+    static String[][] myTasks = new String[100][2];
     static int num = 0;
 
     public static void main(String[] args) {
@@ -13,8 +13,11 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         inData = scan.nextLine();
         while (!inData.equals("bye")) {
+            String split[] = inData.split(" ");
             if (inData.equals("list")) {
                 listTask();
+            } else if (split[0].equals("done")) {
+                updateTask(Integer.parseInt(split[1]));
             } else {
                 addTask(inData);
                 num ++;
@@ -34,6 +37,7 @@ public class Duke {
         printLine();
         System.out.println("");
     }
+
 
     public static void printBye() {
         printLine();
@@ -55,21 +59,41 @@ public class Duke {
 
     public static void listTask() {
         printLine();
+        printIndent();
+        System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < num; i++) {
+            String status = "x";
+            if (myTasks[i][1].equals("true")) {
+                status = "/";
+            }
+
             printIndent();
             int j = i + 1;
-            System.out.println(j + ". " + myTasks[i]);
+            System.out.println(j + ".["  + status + "] "+ myTasks[i][0]);
         }
         printLine();
     }
 
     public static void addTask(String task) {
-        myTasks[num] = task;
+        myTasks[num][0] = task;
+        myTasks[num][1] = "false";
         printLine();
         printIndent();
         System.out.println("added: " + task);
         printLine();
     }
 
+    public static void updateTask(int index) {
+        myTasks[index - 1][1] = "true";
+
+        printLine();
+        printIndent();
+        System.out.println("Nice! I've marked this task as done: ");
+        printIndent();
+        System.out.println("  [/] "+ myTasks[index - 1][0]);
+        printLine();
+    }
+
 }
+
 
